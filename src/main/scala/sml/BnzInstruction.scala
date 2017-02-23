e37a3c36a2b543cbbf300fd44ac77b8ed87a1089
BBK-PiJ-2016-22 <caspertellerup@gmail.com>
2017-02-23 09:55:39 +0000

    Scond commit

--- src/main/scala/sml/BnzInstruction.scala
+++ src/main/scala/sml/BnzInstruction.scala
@@ -4,13 +4,18 @@ package sml
   * Created by Casper on 22/02/2017.
   */
 class BnzInstruction(label: String, op: String, val register: Int, val goto: String) extends Instruction(label,  op) {
-  override def execute(m: Machine): Unit = ???
+  override def execute(m: Machine): Unit = {
+    if(m.regs(register) == 0){
+
+    }
+  }
 
   override def toString(): String = super.toString()
 
 }
 
 object BnzInstruction{
+
   def apply(label: String, register: Int, goto: String): Unit ={
     new BnzInstruction(label, "bnz",register, goto)
   }
